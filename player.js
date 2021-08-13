e1731e23b4f71460133288d16c06997a605c6b88
Calvinn Tallent <calvintallentspam@gmail.com>
2021-08-13 10:47:48 -0700

    Move the keyControl function to the player class, since only the player needs it.

--- player.js
+++ player.js
@@ -4,3 +4,11 @@ function Player(x,y,width,height){
 
 
 
+Player.prototype.keyControl=function(input){
+	this.xVel=0;
+	this.yVel=0;
+	if(input.left)this.xVel=-0.1;
+	if(input.right)this.xVel=0.1;
+	if(input.up)this.yVel=-0.1;
+	if(input.down)this.yVel=0.1;
+}
