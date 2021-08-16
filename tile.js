eb743dbfd424c566bab552d377c49bd10e338b44
Calvinn Tallent <calvintallentspam@gmail.com>
2021-08-15 20:00:22 -0700

    Add the type value, used for drawing and eventually collision detection.

--- tile.js
+++ tile.js
@@ -1,7 +1,8 @@
-function Tile(x,y,width,height,color){
+function Tile(x,y,width,height,color,type){
 	this.x=x;
 	this.y=y;
 	this.color=color;
 	this.width=width;
 	this.height=height;
+	this.type=type;
 }
