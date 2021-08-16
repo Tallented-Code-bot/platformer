bec942cecc6b0cbcb80912c4a9f12d5169916476
Calvinn Tallent <calvintallentspam@gmail.com>
2021-08-15 20:01:05 -0700

    Make a few changes to the world generation.

--- world.js
+++ world.js
@@ -3,7 +3,7 @@ function World(){
 	this.width=30;//the world width in tiles
 	this.height=10;//the world height in tiles
 
-	this.camera=new Camera(20,20);
+	this.camera=new Camera(20,10);
 	this.populateTiles();	
 	this.player=new Player(3,3,0.9,0.9);
 	this.input=new Input();
@@ -24,18 +24,21 @@ World.prototype.populateTiles=function(){
 	for(let x=0;x<this.width;x++){
 		this.tiles.push([]);
 		for(let y=0;y<this.height;y++){
-			let color
+			let color;
+			let type;
 			if(x===0||x===this.width-1||y===0||y===this.height-1){//this makes a black border
 				color="black";
 			}
 			else{
 				if(Math.random()<0.25){
-					color="black"
+					color="black";
+					type="dirt";
 				}else{
-					color="white"
+					color="white";
+					type="sky";
 				}
 			}
-			this.tiles[x].push(new Tile(x,y,this.width,this.height,color));
+			this.tiles[x].push(new Tile(x,y,this.width,this.height,color,type));
 		}
 	}	
 }
