cfefcb51ba731d6ddf3e8c9d0f56d912d5cb03e2
Calvinn Tallent <calvintallentspam@gmail.com>
2021-08-15 20:01:40 -0700

    Render textures instead of black squares for tiles.

--- camera.js
+++ camera.js
@@ -1,4 +1,10 @@
 function Camera(width,height){
+	this.image=new Image();
+	this.image.src="/images/game_tiles.png";
+	this.imageLoadedYet=false;
+	this.image.addEventListener("load",(event)=>{
+		this.imageLoadedYet=true;
+	})
 	this.x=0;//the camera x position, in tiles
 	this.y=0;//the camera y position, in tiles
 	this.width=width;//the camera width in tiles
@@ -9,6 +15,15 @@ function Camera(width,height){
 	this.tileHeight=32;//the height in pixels for 1 tile
 	this.canvas.width=this.width*this.tileWidth;
 	this.canvas.height=this.height*this.tileHeight;
+	this.tileTextures={
+		dirt:[32,32],
+		sky:[96,0],
+		grass_top:[32,32],
+		grass_top_right:[64,0],
+		grass_top_left:[0,0],
+		grass_left:[0,32],
+		grass_right:[64,32]
+	}
 }
 
 
@@ -23,17 +38,32 @@ Camera.prototype.render=function(world){
 		for(let y=this.y;y<this.height+this.y;y++){
 			if((Math.floor(x)>world.width-1||x<0)||(y>world.height-1||y<0)){
 				this.context.fillStyle="white";
+				continue;
 			}else{
 				this.context.fillStyle=world.tiles[Math.floor(x)][Math.floor(y)].color;
 			}
-			
+
 			let toDraw={
 				x:(x-this.x-(this.x-Math.trunc(this.x)))*this.tileWidth,
 				y:(y-this.y)*this.tileHeight,
 				width:this.tileWidth,
-				height:this.tileHeight
+				height:this.tileHeight,
 			}
-			this.context.fillRect(toDraw.x,toDraw.y,toDraw.width,toDraw.height);
+
+			try{
+				toDraw.sourceX=this.tileTextures[world.tiles[Math.floor(x)][Math.floor(y)].type][0];
+				toDraw.sourceY=this.tileTextures[world.tiles[Math.floor(x)][Math.floor(y)].type][1];
+			}
+			catch{
+				
+			}
+			
+			if(this.imageLoadedYet){
+				this.context.drawImage(this.image,toDraw.sourceX,toDraw.sourceY,32,32,toDraw.x,toDraw.y,toDraw.width,toDraw.height);
+
+			}else{
+				this.context.fillRect(toDraw.x,toDraw.y,toDraw.width,toDraw.height);
+			}
 			//the formula to position tiles correctly
 			//the formula should be (tile x*32)-(camera x*32)
 			//this makes sense
